257f26e151233ee06eea89f74ed0abeca8296836
sshkut <sgshkut@gmail.com>
2020-03-04 13:34:01 +0100

    Changes after code review.

--- src/com/foxminded/anagrams/Main.java
+++ src/com/foxminded/anagrams/Main.java
@@ -1,16 +1,25 @@
 package com.foxminded.anagrams;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		String stringForProcessing = "";
-		ReverseStringProcessor anagramProcessor = new ReverseStringExcludeSpecialCharactersProcessor();
-		StringProcessor stringProcessor = new StringProcessor(anagramProcessor);
+		Scanner input = new Scanner(System.in);
+		Anagram anagram = new Anagram();
 		
-		String result = stringProcessor.process(stringForProcessing);
+		System.out.println("Please enter words for reversing.");
+
 		
-		System.out.println(result);
+		while (input.hasNext()) {
+			StringBuilder result = new StringBuilder();
+			String words = input.nextLine();
+			result.append(anagram.createAnagram(words));
+			System.out.println(result.toString());			
+		}
+				
+		input.close();
 	}
 
 }
